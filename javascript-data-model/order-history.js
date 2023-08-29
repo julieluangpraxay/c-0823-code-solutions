478fc2013a6a451777d494ebef3c4c76eb37bbfc
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-29 18:55:20 +0000

    Updated price to number

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,7 +1,7 @@
 const orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
-    total: '$31.55',
+    total: 31.55,
     shipTo: 'JS Masher',
     orderNumber: 'ORDER # 114-3941689-8772232',
     delivered: 'Aug 8, 2020',
@@ -9,11 +9,11 @@ const orderHistory = [
     productTitle: 'JavaScript for impatient programmers',
     seller: 'Rauschmayer, Dr. Axel',
     returnWindow: 'Return window closed on Sep 7, 2020',
-    price: '$34.00',
+    price: 34.0,
   },
   {
     orderPlaced: 'July 19, 2020',
-    total: '$41.33',
+    total: 41.33,
     shipTo: 'JS Masher',
     orderNumber: 'ORDER # 113-9984268-1280257',
     delivered: 'Jul 20, 2020',
@@ -22,11 +22,11 @@ const orderHistory = [
     productTitle: 'The Timeless Way of Building',
     seller: 'Alexander, Christopher',
     returnWindow: 'Return window closed on Aug 19, 2020',
-    price: '$44.53',
+    price: 44.53,
   },
   {
     orderPlaced: 'July 4, 2020',
-    total: '$17.22',
+    total: 17.22,
     shipTo: 'JS Masher',
     orderNumber: 'ORDER # 114-2875557-9059409',
     delivered: 'Jul 7, 2020',
@@ -36,12 +36,12 @@ const orderHistory = [
       'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
     seller: null,
     returnWindow: 'Return window closed on Aug 5, 2020',
-    price: '$15.98',
+    price: 15.98,
   },
   [
     {
       orderPlaced: 'July 3, 2020',
-      total: '$138.93',
+      total: 138.93,
       shipTo: 'JS Masher',
       orderNumber: 'ORDER # 113-2883177-2648248',
       delivered: 'Jul 5, 2020',
@@ -50,7 +50,7 @@ const orderHistory = [
         'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
       seller: null,
       returnWindow: 'Return window closed on Aug 4, 2020',
-      price: '$94.95',
+      price: 94.95,
     },
     {
       orderPlaced: 'July 3, 2020',
@@ -62,7 +62,7 @@ const orderHistory = [
       productTitle: 'The Art of Sql',
       seller: null,
       returnWindow: 'Return window closed on Aug 4, 2020',
-      price: '$33.99',
+      price: 33.99,
     },
   ],
 ];
