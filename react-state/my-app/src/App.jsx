89a54338c17e4a04dc58603d5c4adb7561417ab3
julieluangpraxay <julieluangpraxay@gmail.com>
2023-10-16 03:44:15 +0000

    Completed react-state

--- react-state/my-app/src/App.jsx
+++ react-state/my-app/src/App.jsx
@@ -1,35 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import ToggleButton from './ToggleButton';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <ToggleButton text="Btn 1" color="red" />
+      <ToggleButton text="Btn 2" color="green" />
+      <ToggleButton text="Btn 3" color="blue" />
+    </div>
   );
 }
-
-export default App;
