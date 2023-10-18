0e67fdd2e46c8deb61f817df75a50eaba61e9eb6
julieluangpraxay <julieluangpraxay@gmail.com>
2023-10-18 05:10:03 +0000

    Completed es6-async-await

--- es6-async-await/exceptions.js
+++ es6-async-await/exceptions.js
@@ -5,52 +5,45 @@ import read from './read.js';
 const startTime = Date.now();
 const elapsed = () => `${Math.round((Date.now() - startTime) / 1000)}s -`;
 
-function throwOnce() {
+async function throwOnce() {
   // Note: In the `catch` we are logging just `error.message` for illustration
   // purposes. In actual code you will want to log the entire error so that
   // you get the stack trace.
-  return read('foo', false)
-    .then((msg) => console.log(elapsed(), 'throwOnce:', msg))
-    .catch((error) =>
-      console.log(elapsed(), 'throwOnce Error:', error.message)
-    );
+  try {
+    const msg = await read('foo', false);
+    console.log(elapsed(), 'throwOnce:', msg);
+  } catch (error) {
+    console.log(elapsed(), 'throwOnce Error:', error.message);
+  }
 }
 
-function throwSeveral() {
+async function throwSeveral() {
   // Note: In the `catch` we are logging just `error.message` for illustration
   // purposes. In actual code you will want to log `error` so that
   // you get the stack trace.
-  return read('foo1', false)
-    .then((msg) => {
-      console.log(elapsed(), 'throwSeveral1:', msg);
-      return read('foo2', false);
-    })
-    .then((msg) => {
-      console.log(elapsed(), 'throwSeveral2:', msg);
-      return read('foo3', false);
-    })
-    .then((msg) => console.log(elapsed(), 'throwSeveral3:', msg))
-    .catch((error) =>
-      console.log(elapsed(), 'throwSeveral Error:', error.message)
-    );
+  try {
+    const msg = await read('foo1', false);
+    console.log(elapsed(), 'throwSeveral1:', msg);
+    const msg2 = await read('foo2', false);
+    console.log(elapsed(), 'throwSeveral2:', msg2);
+    const msg3 = await read('throwSeveral3', false);
+    console.log(elapsed(), 'throwSeveral3:', msg3);
+  } catch (error) {
+    console.log(elapsed(), 'throwSeveral Error:', error.message);
+  }
 }
 
-function throwChained() {
-  return read('foo-chain', false)
-    .then((msg1) => {
-      console.log(elapsed(), 'throwChained1:', msg1);
-      return read(msg1, false);
-    })
-    .then((msg2) => {
-      console.log(elapsed(), 'throwChained2:', msg2);
-      return read(msg2, false);
-    })
-    .then((msg3) => console.log(elapsed(), 'throwChained3:', msg3))
-    .catch((error) =>
-      console.log(elapsed(), 'throwChained Error:', error.message)
-    );
+async function throwChained() {
+  try {
+    const msg1 = await read('foo-chain', false);
+    console.log(elapsed(), 'throwChained1:', msg1);
+    const msg2 = await read('rejected foo1', false);
+    console.log(elapsed(), 'throwChained2:', msg2);
+  } catch (error) {
+    console.log(elapsed(), 'throwChained Error:', error.message);
+  }
 }
 
-throwOnce()
-  .then(() => throwSeveral())
-  .then(() => throwChained());
+await throwOnce();
+await throwSeveral();
+await throwChained();
