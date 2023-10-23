7dfaa5766b8180f3ce06405e221abcd25715c2f8
julieluangpraxay <julieluangpraxay@gmail.com>
2023-10-23 00:20:08 +0000

    WIP

---
+++ express-get-json/server.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+const app = express();
+const port = 8080;
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = Object.values(grades);
+  res.json(gradesArray);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
