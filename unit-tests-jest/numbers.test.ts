7d7f83a6fc701d01ccdb4570e5d3d5d774c43e04
julieluangpraxay <julieluangpraxay@gmail.com>
2023-11-14 00:20:07 +0000

    Completed unit-tests-jest

--- unit-tests-jest/numbers.test.ts
+++ unit-tests-jest/numbers.test.ts
@@ -1,10 +1,63 @@
 // Ensure correct import statement at the beginning of your test file
-import { evenNumbers } from './numbers';
+import { divideBy, evenNumbers, multiplyBy, toDollars } from './numbers';
 
 describe('evenNumbers', () => {
   it('returns the even numbers', () => {
     const numbers = [1, 4, 5, 10, 0];
     const result = evenNumbers(numbers);
     expect(result).toEqual([4, 10, 0]);
+  });
+  it('empty array', () => {
+    const numbers: number[] = [];
+    const result = evenNumbers(numbers);
+    expect(result).toEqual([]);
+  });
+  it('no even numbers', () => {
+    const numbers: number[] = [1, 3, 5, 7];
+    const result = evenNumbers(numbers);
+    expect(result).toEqual([]);
+  });
+});
+
+describe('ToDollars', () => {
+  it('turns the number into dollars with two decimals', () => {
+    const number = 5.0;
+    const result = toDollars(number);
+    expect(result).toEqual('$5.00');
+  });
+  it('turns 0 into dollars with two decimals after', () => {
+    const number = 0;
+    const result = toDollars(number);
+    expect(result).toEqual('$0.00');
+  });
+});
+
+describe('divideBy', () => {
+  it('Returns a new array of numbers where every entry has been divided by the given divisor.', () => {
+    const numbers = [10, 20, 30, 40];
+    const divisor = 1;
+    const result = divideBy(numbers, divisor);
+    expect(result).toEqual([10, 20, 30, 40]);
+  });
+});
+
+describe('multiplyBy', () => {
+  it('Modifies an object by multiplying the value of each key, but only if that value is a number.', () => {
+    const numbersObject = {
+      key1: 2,
+      key2: 'string',
+      key3: 3.5,
+      key4: null,
+      key5: '5',
+    };
+    const multiplier = 2;
+    const result = multiplyBy(numbersObject, multiplier);
+    expect(result).toEqual({
+      key1: 4,
+      key2: 'string',
+      key3: 7,
+      key4: null,
+      key5: '5',
+    });
   });
 });
--- unit-tests-jest/numbers.ts
+++ unit-tests-jest/numbers.ts
@@ -2,14 +2,14 @@
  * Returns a new array that contains only the even numbers in the given array.
  */
 export function evenNumbers(numbers: number[]): number[] {
-  return numbers.filter((n) => n % 2);
+  return numbers.filter((n) => n % 2 === 0);
 }
 
 /**
  * Returns a number formatted in dollars and cents.
  */
 export function toDollars(amount: number): string {
-  return `$${amount}.00`;
+  return `$${amount.toFixed(2)}`;
 }
 
 /**
@@ -17,7 +17,7 @@ export function toDollars(amount: number): string {
  * given divisor. Does not modify the original array.
  */
 export function divideBy(numbers: number[], divisor: number): number[] {
-  for (let i = 1; i < numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     numbers[i] = numbers[i] / divisor;
   }
   return numbers;
@@ -33,7 +33,11 @@ export function multiplyBy(
 ): Record<string, unknown> {
   const result: Record<string, unknown> = {};
   Object.entries(obj).forEach(([key, value]) => {
-    result[key] = Number(value) * multiplier + 1;
+    if (typeof value === 'number') {
+      result[key] = Number(value) * multiplier;
+    } else {
+      result[key] = value;
+    }
   });
   return result;
 }
